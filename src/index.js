12140c2dda9cfadc7df73ff6a8e04527509d5afc
biancanoelle <111887171+biancanoelle@users.noreply.github.com>
2022-11-06 12:21:41 -0500

    update
    updated api url

--- src/index.js
+++ src/index.js
@@ -58,36 +58,13 @@ function retrievePosition(position) {
 
 navigator.geolocation.getCurrentPosition(retrievePosition);
 
-var inputval = document.querySelector("#cityinput");
-var btn = document.querySelector("#add");
-var city = document.querySelector("#cityoutput");
-var descrip = document.querySelector("#description");
-var temp = document.querySelector("#temp");
-var wind = document.querySelector("#wind");
-apik = "3045dd712ffe6e702e3245525ac7fa38";
-function convertion(val) {
-  return (val - 273).toFixed(2);
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let apiKey = "3499ef150985eccadd080ff408a018df";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let units = "imperial";
+  let url =
+    "api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={apiKey}";
+  forecastElement.innerHTML = "Forecast";
 }
-
-btn.addEventListener("click", function () {
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      inputval.value +
-      "&appid=" +
-      apik
-  )
-    .then((res) => res.json())
-
-    .then((data) => {
-      var nameval = data["name"];
-      var descrip = data["weather"]["0"]["description"];
-      var tempature = data["main"]["temp"];
-      var wndspd = data["wind"]["speed"];
-      city.innerHTML = `Weather of <span>${nameval}<span>`;
-      temp.innerHTML = `Temperature: <span>${convertion(tempature)} C</span>`;
-      description.innerHTML = `Sky Conditions: <span>${descrip}<span>`;
-      wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`;
-    })
-
-    .catch((err) => alert("You entered Wrong city name"));
-});
